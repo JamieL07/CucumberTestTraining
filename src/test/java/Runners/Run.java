e38237356b047ff3b7971d2aa18d9fe773c73ace
Jamie Hart <jamie.hart@kerv.com>
2022-07-28 00:09:37 +0100

    RunnerExamples

--- src/test/java/Runners/Run.java
+++ src/test/java/Runners/Run.java
@@ -4,8 +4,14 @@ import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
 @CucumberOptions(
-        features = "src/test/resources/features/firstTest.feature",
-        glue = {"StepDefinitions"}
+        features = "src/test/resources/features/",
+        glue = {"StepDefinitions"},
+        monochrome = true,
+        plugin = {"pretty", "html:target/cucumber-report.html",
+                "json:target/JSONReport/report.json",
+                "junit:target/JunitReport/report.xml"},
+        tags = "@smoketest"
+
 )
 public class Run extends AbstractTestNGCucumberTests {
 }
